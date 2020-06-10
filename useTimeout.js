a17a5cbdcaaddb3c152d9321d66659724656ad89
trinadhkalangi <trinadhkalangi@users.noreply.github.com>
2020-06-10 22:18:02 +0530

    usePrevious Hook

--- useTimeout.js
+++ useTimeout.js
@@ -1,3 +1,5 @@
+// With this hook, we can implement setTimeout using a declarative approach. First, we create a custom hook with a callback and a delay. Then we use the useRef hook to create a ref for the callback function. Finally, we make use of useEffect twice. One time for remembering the last callback and one time for setting up the timeout and cleaning up.
+
 const useTimeout = (callback, delay) => {
     const savedCallback = React.useRef();
 
